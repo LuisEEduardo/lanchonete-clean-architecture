c7195b6a6bd579556a4456f8a3ea5f9ba919a7b7
LuisEEduardo <luiseduardolemos282@gmail.com>
2023-01-15 18:31:57 -0300

    Implements the infra ioc

---
+++ src/Lanchonete.Infra.Ioc/Boostrap.cs
@@ -0,0 +1,16 @@
+using Lanchonete.Domain.Interfaces.Repositories;
+using Lanchonete.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lanchonete.Infra.Ioc
+{
+    public class Boostrap
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<ILancheRepository, LancheRepository>();
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
+        }
+    }
+}
